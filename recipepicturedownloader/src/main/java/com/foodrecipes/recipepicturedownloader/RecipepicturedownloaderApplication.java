52671128652bb0ff160bb2788ce04cac124c039a
BatuUzun <uzunbatu@hotmail.com>
2024-07-20 20:25:37 +0300

    Recipe downloader

---
+++ recipepicturedownloader/src/main/java/com/foodrecipes/recipepicturedownloader/RecipepicturedownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.foodrecipes.recipepicturedownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipepicturedownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipepicturedownloaderApplication.class, args);
+	}
+
+}
