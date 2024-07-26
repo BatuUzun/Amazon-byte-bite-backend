30d42845c2687f7a489ae97113d3e30fb319bf85
BatuUzun <uzunbatu@hotmail.com>
2024-07-26 12:15:24 +0300

    Comments sorted

--- comment/src/main/java/com/foodrecipes/comment/service/CommentService.java
+++ comment/src/main/java/com/foodrecipes/comment/service/CommentService.java
@@ -1,11 +1,13 @@
 package com.foodrecipes.comment.service;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
 import com.foodrecipes.comment.constants.Constants;
-import com.foodrecipes.comment.entity.Comment;
 import com.foodrecipes.comment.entity.CommentProjection;
 import com.foodrecipes.comment.repository.CommentRepository;
 
@@ -20,7 +22,13 @@ public class CommentService {
     }
     
     public List<CommentProjection> findByRecipeId(Long recipeId, int page) {
-    	PageRequest pageRequest = PageRequest.of(page, Constants.PAGE_SIZE);
+    	
+    	Sort sort = Sort.by(Sort.Order.asc("dateCreated"));
+
+        // Create a Pageable instance with sorting
+        PageRequest pageRequest = PageRequest.of(page, Constants.PAGE_SIZE, sort);
+    	
+    	//PageRequest pageRequest = PageRequest.of(page, Constants.PAGE_SIZE);
         return commentRepository.findByRecipeId(recipeId, pageRequest);
     }
 }
